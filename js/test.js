7f1526f4c9cea444f25daa41f9fb82320ef9f350
Ansonxddd <69643533+ansonlawss@users.noreply.github.com>
2021-07-21 03:29:24 +0800

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,33 @@
+function Table1()
+{
+     var elem=document.getElementById("TableNested1");
+     var hide = elem.style.display =="none";
+     if (hide) {
+         elem.style.display="table";
+    } 
+    else {
+       elem.style.display="none";
+    }
+}
+function Table2()
+{
+     var elem=document.getElementById("TableNested2");
+     var hide = elem.style.display =="none";
+     if (hide) {
+         elem.style.display="table";
+    } 
+    else {
+       elem.style.display="none";
+    }
+}
+function Table3()
+{
+     var elem=document.getElementById("TableNested3");
+     var hide = elem.style.display =="none";
+     if (hide) {
+         elem.style.display="table";
+    } 
+    else {
+       elem.style.display="none";
+    }
+}
